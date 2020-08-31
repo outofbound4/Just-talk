01a847f485c0cd226a4a42e4e21d08dff6c16046
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-08-31 13:09:17 +0530

    File created

---
+++ src/javascript/database.js
@@ -0,0 +1,26 @@
+//code init
+const mongoose = require('mongoose');
+const assert = require('assert');
+const config = require('./../../config');
+
+// mongoDB_URL = 'mongodb://127.0.0.1:27017/chatapp', 'chatapp' is database name
+var mongoDB_URL = config.MongoDB.MongoDB_URL + config.MongoDB.DATABASE_NAME;
+
+//  Connecting to the database
+const connect = async function () {
+    console.log('Connecting to DB - uri: ' + mongoDB_URL);
+    return mongoose.connect(
+        mongoDB_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }
+    );
+};
+
+connect().then(() => {
+    console.log('Database connection successful');
+}).catch((e) => {
+    console.log('handle error here: ', e.message)
+});
