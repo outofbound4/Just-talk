e1f49c84d5bd56be9ab4d770e7351c7dd6b8bf11
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-08-31 15:35:23 +0530

    File updated

--- src/controller/UserAuthController.js
+++ src/controller/UserAuthController.js
@@ -1,4 +1,4 @@
-
+var User = require('../model/User');
 /**
  * Class ViewController Controller
  */
@@ -7,19 +7,67 @@ class UserAuthController {
      * Liste of Articles
      * @param {*} req
      * @param {*} res
-     */
+    */
+    constructor(validationResult) {
+        this.validationResult = validationResult;
+    }
 
     register(req, res) {
-        // data.push(req.query);
-        // return res.json(req.query);
-        console.log(req.body);
+        let errors = this.validationResult(req);
+        /* If some error occurs, then this 
+         *  block of code will run 
+         */
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            return res.json(errors);
+        }
+        /* If no error occurs, then this 
+         * block of code will run 
+         */
+        else {
+            User.create({
+                name: req.body.name,
+                mobile: req.body.mobile,
+                password: req.body.password,
+            },
+            
+            function (error, result) {
+                // iff error occurs
+                if (error) {
+                    console.log(error);
+                    return res.json({
+                        status: false,
+                        message: 'Error in inserting data in user Module',
+                        error: error
+                    });
+                }
+                // if every things ok
+                console.log(result);
+                return res.json({
+                    status: true,
+                    message: 'inserted data in user Module',
+                    result: result
+                });
+            });
+        }
     }
 
     login(req, res) {
-        // console.log(req);
-        // data.push(req.body);
-        // return res.json(req.body);
-        console.log(req.body);
+        let errors = this.validationResult(req);
+        /* If some error occurs, then this 
+         *  block of code will run 
+         */
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            return res.json(errors);
+        }
+        /* If no error occurs, then this 
+        * block of code will run 
+        */
+        else {
+            res.send("Successfully validated");
+            console.log(req.body);
+        }
     }
 }
 
