b21a2e4e924bf45c4805d5c03b5affbbd4bfc1e2
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-09-01 22:55:45 +0530

    File updated

--- src/model/User.js
+++ src/model/User.js
@@ -2,26 +2,50 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    // _id: { type: String, required: true },
-    name: { type: String, required: true },
-    email: {
-        type: String,
-        unique: true,
-        lowercase: true,
-    },
-    mobile: {
+    name: {
         type: String,
         required: true,
-        unique: true,
     },
-    profile_pic: { type: String },
-    email_verified_at: { type: Date },
-    mobile_verified_at: { type: Date },
-    password: { type: String, required: true },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date },
-    remember_token: { type: String },
-    status: { type: Boolean },
+    // email: {
+    //     type: String,
+    //     unique: true,
+    //     lowercase: true,
+    // },
+    mobile: {
+        type: String,
+        require: true,
+        index: true,
+        unique: true,
+        sparse: true,
+    },
+    profile_pic: {
+        type: String,
+    },
+    email_verified_at: {
+        type: Date,
+    },
+    mobile_verified_at: {
+        type: Date,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+    remember_token: {
+        type: String,
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    },
 
 });
 
