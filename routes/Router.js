441f07afae7c829b6584d8e9ff697cc4b38857c1
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-09-05 11:17:00 +0530

    File updated

--- routes/Router.js
+++ routes/Router.js
@@ -12,7 +12,13 @@ var UserAuthController = require('../src/controller/UserAuthController');
 const UserAuthController_obj = new UserAuthController(validationResult);
 
 router.get("/home", (req, res) => Viewcontroller_obj.home(req, res));
+router.get("/chatbox", (req, res) => Viewcontroller_obj.chatbox(req, res));
+router.get("/contactlist", (req, res) => Viewcontroller_obj.contactlist(req, res));
+router.get("/ownprofile", (req, res) => Viewcontroller_obj.ownprofile(req, res));
+router.get("/otherprofile", (req, res) => Viewcontroller_obj.otherprofile(req, res));
 router.get("/userAuth", (req, res) => Viewcontroller_obj.userAuth(req, res));
+router.get("/editprofile", (req, res) => Viewcontroller_obj.editprofile(req, res));
+
 //route for registration og new users
 router.post("/register", [
     //check not empty fields
