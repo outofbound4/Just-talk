e203feac9121f311371b61ad23642893c8a54b39
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-09-17 21:38:48 +0530

    File updated

--- socket/socket.js
+++ socket/socket.js
@@ -1,27 +1,39 @@
 // socket connection
 var Socket = require("socket.io");
 var io;
+var activeUsers = [];
 class SocketConnection {
-    // constructor 
 
+    // constructor 
     constructor(server) {
         io = Socket(server);
     }
-
+    // this function called in app.js
     listenConnection() {
-        // console.log(this.io);
         io.on('connection', function (Socket) {
             console.log("made Socket connection id : " + Socket.id);
-            Socket.on('chat', function (data) {
-                io.sockets.emit('chat', data);
+
+            // here sending message to a particular person
+            Socket.on('sending_message', function (data) {
+                io.to(activeUsers[data.id_user2]).emit("receiving_message", data);
             });
-            Socket.on('typing', function (data) {
-                Socket.broadcast.emit('typing', data);
+            // called when user type message to another user
+            Socket.on('typing_on', function (data) {
+                Socket.to(activeUsers[data.id_user2]).emit('typing_on', data);
             });
-            Socket.on('remove_typing', function (data) {
-                Socket.broadcast.emit('remove_typing', data);
+            // called when user type backspace
+            Socket.on('typing_off', function (data) {
+                Socket.to(activeUsers[data.id_user2]).emit('typing_off', data);
             });
-            
+            // called after user login or on chat page load
+            Socket.on("new user", function (data) {
+                activeUsers[data] = Socket.id;
+                io.to(activeUsers[data]).emit("new user", data);
+            });
+
+            Socket.on("disconnect", () => {
+
+            });
         });
     }
 }
