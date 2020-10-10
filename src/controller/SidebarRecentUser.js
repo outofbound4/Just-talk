53264c88cf1cd8184150e5fbee0e0fb2190be7a2
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-10-10 19:49:36 +0530

    File updated

--- src/controller/SidebarRecentUser.js
+++ src/controller/SidebarRecentUser.js
@@ -32,18 +32,33 @@ class SidebarRecentUser {
          */
 
         User.find({ '_id': req.body.id_user1 }, function (error, result) {
-            // Map the result[0].recent_user into an array of just the userid
-            var ids = result[0].recent_user.map(function (doc) { return doc.userid; });
-            // Get the User whose _id are in that set.
-            User.find({ '_id': { $in: ids } }, '_id name profile_pic', function (error, result) {
-                // result contains your answer
-                // console.log(result);
-                return res.json({
-                    status: 200,
-                    message: 'data fetching successful',
-                    result: result,
-                });
+
+            let sortedArray = result[0].recent_user.sort(function (a, b) {
+                return new Date(b.time) - new Date(a.time);
             });
+            // Map the result[0].recent_user into an array of just the userid
+            var ids = sortedArray.map(function (doc) { return doc.userid; });
+            // Get the User whose _id are in that set.
+            var data = [];
+            var messages = [];
+            for (let i = 0; i < ids.length; i++) {
+                User.findOne({ '_id': ids[i] }, '_id name profile_pic', function (error, result) {
+                    // result contains the user of recent list
+                    // here we are inserting the recent message to the result JSON object
+                    messages[i] = sortedArray[i].message;
+                    data.push(result);
+                    // it doesn't send data to untill all the data are fetched
+                    if (i == ids.length - 1) {
+                        // sending sidebar data to user
+                        return res.json({
+                            status: 200,
+                            message: 'data fetching successful',
+                            result: data,
+                            messages: messages,
+                        });
+                    }
+                });
+            }
         });
     }
 }
