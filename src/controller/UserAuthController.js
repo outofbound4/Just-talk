6f035cf4724e50adba490b3dfda788efdbc7e146
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-10-24 21:06:31 +0530

    File updated

--- src/controller/UserAuthController.js
+++ src/controller/UserAuthController.js
@@ -123,44 +123,6 @@ class UserAuthController {
         }
     }
 
-    // it will send userdata by its id
-    getUserById(req, res) {
-        let errors = this.validationResult(req);
-        /* If some error occurs, then this 
-         *  block of code will run 
-         */
-        if (!errors.isEmpty()) {
-            //sending errors to client page
-            return res.json({
-                status: 400,
-                message: 'Required Param Empty',
-                errors: errors,
-            });
-        }
-        /* If no error occurs, then this 
-        * block of code will run 
-        */
-        else {
-            //finding the user credentials
-            User.find({ '_id': req.body.id_user1 }, '_id name profile_pic', function (error, result) {
-                if (error) {
-                    //sending errors to client page
-                    return res.json({
-                        status: 500,
-                        message: 'data fetching error occurs',
-                        error: error,
-                    });
-                }
-                // if everything ok
-                //  returning data to user
-                return res.json({
-                    status: 200,
-                    message: 'Authorised user',
-                    result: result[0],
-                });
-            });
-        }
-    }
     // logout function here we destoy the session
     logout(req, res) {
         //  returning data to user
