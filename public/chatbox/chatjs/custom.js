02593a04898c976fef4dace3403f7f6a752a9b16
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-10-26 14:30:58 +0530

    File updated

--- public/chatbox/chatjs/custom.js
+++ public/chatbox/chatjs/custom.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $(".e1").click(function(event){
+    $(".e1").click(function (event) {
         var client = $('.chat.active-chat').attr('client');
 
         var prevMsg = $('#chatFrom .chatboxtextarea').val();
         var shortname = $(this).data('shortname');
 
-        $('#chatFrom .chatboxtextarea').val(prevMsg+' '+shortname+' ');
+        $('#chatFrom .chatboxtextarea').val(prevMsg + ' ' + shortname + ' ');
         //$('#chatFrom .chatboxtextarea').focus();
     });
-    $(".chat-head .personName").click(function(){
+    $(".chat-head .personName").click(function () {
         var personName = $(this).text();
     });
 
 
 
-    $("#launchProfile").click(function(){
+    $("#launchProfile").click(function () {
         $("#userProfile").html('<div class="preloader"><div class="cssload-speeding-wheel"></div></div>');
 
-        var usname = $(this).find('img').attr('alt');
-
-        var usname = $('.right .top').attr("data-user");
-        var img = $('.right .top').attr("data-image");
-
+        let id_user2 = $("#id_user2").val();
+        let statusbar = $("#" + id_user2).attr("data-statusbar");
+        let email = $("#" + id_user2).attr("data-email");
+        var usname = $('.top2').attr("data-user");
+        var img = $('#launchProfile').find('img').attr('src');
         $('#wchat .wchat').removeClass('two');
         $('#wchat .wchat').addClass('three');
         $('.wchat-three').slideDown(50);
@@ -31,63 +31,59 @@ $(document).ready(function() {
         var profileTpl = '<div class="">' +
             '<div class="user-bg">' +
             '<div class="overlay-box">' +
-        '<div class="user-content"> <a href="javascript:void(0)">' +
-        '<img class="thumb-lg img-circle" src="storage/user_image/'+img+'"></a>' +
-        '<h4 class="text-white">'+usname+'</h4>' +
-        '<h5 class="text-white">'+usname+'@gmail.com</h5>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '<div class="user-btm-box">' +
-        '<div class="row text-center m-t-10">' +
-        '<div class="col-md-6 b-r"><strong>Name</strong><p>'+usname+'</p></div>' +
-        '<div class="col-md-6"><strong>Gender</strong><p>male</p></div>' +
-        '</div>' +
-        '<hr>' +
-        '<div class="row text-center m-t-10">' +
-        '<div class="col-md-12"><strong>Status</strong><p> I Love You <3 </p></div>' +
-        '</div>' +
-        '<hr>' +
-        '<div class="col-md-1 col-sm-1 text-center">&nbsp;</div>' +
-        '</div>' +
-        '</div>';
+            '<div class="user-content"> <a href="javascript:void(0)">' +
+            '<img class="thumb-lg img-circle" src="' + img + '"></a>' +
+            '<h4 class="text-white">' + usname + '</h4>' +
+            '<h5 class="text-white">' + email + '</h5>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '<div class="user-btm-box">' +
+            '<hr>' +
+            '<div class="row text-center m-t-10">' +
+            '<div class="col-md-12"><strong>Status</strong><p> ' + statusbar + ' </p></div>' +
+            '</div>' +
+            '<hr>' +
+            '<div class="col-md-1 col-sm-1 text-center">&nbsp;</div>' +
+            '</div>' +
+            '</div>';
 
         $("#userProfile").html(profileTpl);
     });
 
-    $(".header-close").click(function(){
+    $(".header-close").click(function () {
         $('#wchat .wchat').removeClass('three');
         $('#wchat .wchat').addClass('two');
-        $('.wchat-three').css({'display':'none'});
+        $('.wchat-three').css({ 'display': 'none' });
 
     });
 
-    $(".scroll-down").click(function(){
+    $(".scroll-down").click(function () {
         scrollDown();
     });
 
-    $("#mute-sound").click(function(){
-        if(eval(localStorage.sound)){
+    $("#mute-sound").click(function () {
+        if (eval(localStorage.sound)) {
             localStorage.sound = false;
             $("#mute-sound").html('<i class="icon icon-volume-off"></i>');
         }
-        else{
+        else {
             localStorage.sound = true;
             $("#mute-sound").html('<i class="icon icon-volume-2"></i>');
             audiomp3.play();
             audioogg.play();
         }
     });
-    $("#MobileChromeplaysound").click(function(){
-        if(eval(localStorage.sound)){
+    $("#MobileChromeplaysound").click(function () {
+        if (eval(localStorage.sound)) {
             audiomp3.play();
             audioogg.play();
         }
     });
-    if(eval(localStorage.sound)){
+    if (eval(localStorage.sound)) {
         $("#mute-sound").html('<i class="icon icon-volume-2"></i>');
     }
-    else{
+    else {
         $("#mute-sound").html('<i class="icon icon-volume-off"></i>');
     }
 
@@ -109,16 +105,16 @@ $(document).ready(function() {
 
 
 function chatemoji() {
-    $(".target-emoji").slideToggle( 'fast', function(){
+    $(".target-emoji").slideToggle('fast', function () {
 
         if ($(".target-emoji").css('display') == 'block') {
             //alert($(window).height());
             //$('.chat-list').css({'height':(($(window).height())-279)+'px'});
-            $('.wchat-filler').css({'height':225+'px'});
+            $('.wchat-filler').css({ 'height': 225 + 'px' });
             $('.btn-emoji').removeClass('ti-face-smile').addClass('ti-arrow-circle-down');
         } else {
             //$('.chat-list').css({'height':(($(window).height())-179)+'px'});
-            $('.wchat-filler').css({'height':0+'px'});
+            $('.wchat-filler').css({ 'height': 0 + 'px' });
             $('.btn-emoji').removeClass('ti-arrow-circle-down').addClass('ti-face-smile');
         }
     });
@@ -127,12 +123,11 @@ function chatemoji() {
 
 function typePlace() {
 
-    if(!$('#textarea').html() == '')
-    {
-        $(".input-placeholder").css({'visibility':'hidden'});
+    if (!$('#textarea').html() == '') {
+        $(".input-placeholder").css({ 'visibility': 'hidden' });
     }
-    else{
-        $(".input-placeholder").css({'visibility':'visible'});
+    else {
+        $(".input-placeholder").css({ 'visibility': 'visible' });
     }
 
 }
@@ -140,14 +135,14 @@ function typePlace() {
 
 
 //Inbox User search
-$(document).ready(function(){
-    $('.contact-list li').each(function(){
+$(document).ready(function () {
+    $('.contact-list li').each(function () {
         $(this).attr('data-search-term', $(this).text().toLowerCase());
     });
 
-    $('.live-search-box').on('keyup', function(){
+    $('.live-search-box').on('keyup', function () {
         var searchTerm = $(this).val().toLowerCase();
-        $('.live-search-list li').each(function(){
+        $('.live-search-list li').each(function () {
 
             if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
                 $(this).show();
@@ -158,7 +153,7 @@ $(document).ready(function(){
     });
 });
 
-$(window).bind("load", function() {
+$(window).bind("load", function () {
     //$('.person:first').trigger('click');
     var personName = $('.person:first').find('.personName').text();
     $('.right .top .personName').html(personName);
@@ -176,14 +171,14 @@ $(window).bind("load", function() {
         document.execCommand('insertText', false, text);
     });
 */
-    $('.chatboxtextarea').on('focus',function(e) {
-        $(".target-emoji").css({'display':'none'});
-        $('.wchat-filler').css({'height':0+'px'});
+    $('.chatboxtextarea').on('focus', function (e) {
+        $(".target-emoji").css({ 'display': 'none' });
+        $('.wchat-filler').css({ 'height': 0 + 'px' });
     });
 });
 
 
-$('.left .person').mousedown(function(){
+$('.left .person').mousedown(function () {
     if ($(this).hasClass('.active')) {
         return false;
     } else {
@@ -200,7 +195,7 @@ $('.left .person').mousedown(function(){
         $('.chat').removeClass('active-chat');
         $('.left .person').removeClass('active');
         $(this).addClass('active');
-        $('.chat[data-chat = '+findChat+']').addClass('active-chat');
+        $('.chat[data-chat = ' + findChat + ']').addClass('active-chat');
     }
 });
 
