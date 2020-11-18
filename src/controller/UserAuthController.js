16819ae727dec130c8385692fd7f32c20b759f57
GAURAV KUMAR <gaurav.kr109@gmail.com>
2020-11-18 14:19:57 +0530

    File updated

--- src/controller/UserAuthController.js
+++ src/controller/UserAuthController.js
@@ -33,7 +33,7 @@ class UserAuthController {
             let email = req.body.email;
             let password = req.body.password;
             //finding the user credentials
-            User.find({ 'email': email }, "_id mobile name email password statusbar profile_pic", function(errors, result) {
+            User.find({ 'email': email }, "_id mobile name email password statusbar profile_pic email_verified_at", function(errors, result) {
                 if (errors) {
                     //sending errors to client page
                     return res.json({
@@ -45,38 +45,47 @@ class UserAuthController {
                 // if everything ok
                 // checking if user exist with this email or not
                 if (Object.keys(result).length !== 0) {
-                    let _id = result[0]._id;
-                    let mobile = result[0].mobile;
-                    let name = result[0].name;
-                    let email = result[0].email;
-                    let statusbar = result[0].statusbar;
-                    let hash = result[0].password;
-                    let profile_pic = result[0].profile_pic;
-                    // Load hash from your password DB.
-                    bcrypt.compare(password, hash, function(err, Authorised) {
-                        // console.log(result)
-                        if (Authorised === true) {
-                            //setting session
-                            req.session.mobile = mobile;
-                            req.session.name = name;
-                            req.session.profile_pic = profile_pic;
-                            req.session.email = email;
-                            req.session.statusbar = statusbar;
-                            req.session._id = _id;
-                            //  returning data to user
-                            return res.json({
-                                status: 200,
-                                message: 'Authorised user',
-                                // result: result,
-                            });
-                        } else {
-                            //  returning data to user
-                            return res.json({
-                                status: 422,
-                                message: 'Unauthorised user, password incorrect',
-                            });
-                        }
-                    });
+                    // it checks if email is verified or not
+                    if (result[0].email_verified_at != null) {
+                        let _id = result[0]._id;
+                        let mobile = result[0].mobile;
+                        let name = result[0].name;
+                        let email = result[0].email;
+                        let statusbar = result[0].statusbar;
+                        let hash = result[0].password;
+                        let profile_pic = result[0].profile_pic;
+                        // Load hash from your password DB.
+                        bcrypt.compare(password, hash, function(err, Authorised) {
+                            // console.log(result)
+                            if (Authorised === true) {
+                                //setting session
+                                req.session.mobile = mobile;
+                                req.session.name = name;
+                                req.session.profile_pic = profile_pic;
+                                req.session.email = email;
+                                req.session.statusbar = statusbar;
+                                req.session._id = _id;
+                                //  returning data to user
+                                return res.json({
+                                    status: 200,
+                                    message: 'Authorised user',
+                                    // result: result,
+                                });
+                            } else {
+                                //  returning data to user
+                                return res.json({
+                                    status: 422,
+                                    message: 'Unauthorised user, password incorrect',
+                                });
+                            }
+
+                        });
+                    } else {
+                        return res.json({
+                            status: 423,
+                            message: 'Verify email or Register again.',
+                        });
+                    }
                 } else {
                     return res.json({
                         status: 421,
